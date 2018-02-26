34e2da9b714d8493087e4a6dad3ec80b634c21bd
Alfred Gao <alfredg@alfredg.cn>
2018-02-26 15:29:51 +0800

    adjust Package.swift for swift4

--- Package.swift
+++ Package.swift
@@ -1,5 +1,17 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "TextFormater"
+    name: "TextFormater",
+    products: [
+        .library(
+            name: "TextFormater",
+            targets: ["TextFormater"]),
+    ],
+    targets: [
+        .target(
+            name: "TextFormater",
+            path: "Sources"
+            )
+    ]
 )
