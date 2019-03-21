d9175a72141ea0a958af0db9675816cee3382c89
Alfred Gao <alfredg@alfredg.cn>
2019-03-22 02:48:37 +0800

    Examples of new version

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -9,17 +9,27 @@
 import UIKit
 import TextFormater
 
+class TestImageProvider: NSObject,GetImageForTextFormater {
+    public func getImage(byKey : String) -> UIImage? {
+        return #imageLiteral(resourceName: "test.jpg")
+    }
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var labelFormatResult: UILabel!
     @IBOutlet weak var textMarkupString: UITextField!
     
-    let textFormater = TextFormater()
+    let textFormater = TextFormater(fontSize:20,
+                                    boldFontSize:20,
+                                    italicFontSize:20,
+                                    equationFontSize:30,
+                                    imageDelegate: TestImageProvider())
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        textMarkupString.text = "Enter Markup String here."
+        textMarkupString.text = "#Test #测试  ###Markdown * **Bold 粗体** * *Italic 斜体* * NewLine <br> 换行 * <color red>c</color><color blue>o</color><color yellow>l</color><color purple>o</color>c * <bgcolor red>背</bgcolor><bgcolor blue><color white>景</color></bgcolor><bgcolor green>色</bgcolor> ###LaTeX 1. Inline 行内公式 $\\\\frac { x ^ { 2 } + 4 x } { x - 1 } - \\\\frac { 72 x - 72 } { x ^ { 2 } + 4 x } = 18$ Equation 2. 独立公式 $$\\\\ell ( \\\\theta ) = \\\\sum _ { i = 1 } ^ { m } \\\\log p ( x ; \\\\theta )$$"
         labelFormatResult.attributedText=textFormater.parse(textMarkupString.text ?? "")
         
     }
--- ExampleOSX/ExampleOSX/ViewController.swift
+++ ExampleOSX/ExampleOSX/ViewController.swift
@@ -9,19 +9,28 @@
 import Cocoa
 import TextFormater
 
+class TestImageProvider: NSObject,GetImageForTextFormater {
+    public func getImage(byKey : String) -> NSImage? {
+        return #imageLiteral(resourceName: "test.jpg")
+    }
+}
+
 class ViewController: NSViewController {
 
     @IBOutlet weak var labelFormatResult: NSTextField!
     @IBOutlet weak var textMarkupString: NSTextField!
     
-    let textFormater = TextFormater()
+    let textFormater = TextFormater(fontSize:20,
+                                    boldFontSize:20,
+                                    italicFontSize:20,
+                                    equationFontSize:30,
+                                    imageDelegate: TestImageProvider())
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
-        textMarkupString.stringValue = "Enter markup text here"
+        textMarkupString.stringValue = "#Test #测试  ###Markdown * **Bold 粗体** * *Italic 斜体* * NewLine <br> 换行 * <color red>c</color><color blue>o</color><color yellow>l</color><color purple>o</color>c * <bgcolor red>背</bgcolor><bgcolor blue><color white>景</color></bgcolor><bgcolor green>色</bgcolor> ###LaTeX 1. Inline 行内公式 $\\\\frac { x ^ { 2 } + 4 x } { x - 1 } - \\\\frac { 72 x - 72 } { x ^ { 2 } + 4 x } = 18$ Equation 2. 独立公式 $$\\\\ell ( \\\\theta ) = \\\\sum _ { i = 1 } ^ { m } \\\\log p ( x ; \\\\theta )$$"
         labelFormatResult.attributedStringValue = textFormater.parse("Result here will be")
     }
 
